657fcdd60ec5decc0822bf47b84dd50f8cbae672
Voltron42 <dajohnson1978@gmail.com>
2022-10-05 15:55:21 -0500

    initial project commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to facetious-nocturn
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/facetious_nocturn/core_test.clj
@@ -0,0 +1,7 @@
+(ns facetious-nocturn.core-test
+  (:require [clojure.test :refer :all]
+            [facetious-nocturn.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
