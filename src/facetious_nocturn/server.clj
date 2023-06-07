e91fd2f69adcfa1d4849e196f2234a55a8d1278d
Voltron42 <dajohnson1978@gmail.com>
2023-06-07 11:07:02 -0500

    building out session management structure

--- src/facetious_nocturn/server.clj
+++ src/facetious_nocturn/server.clj
@@ -7,29 +7,42 @@
             [ring.util.http-response :as http]
             [environ.core :refer [env]]
             [org.httpkit.server :as server]
-            [schema.core :as s]))
+            [facetious-nocturn.schema :as s]
+            [cheshire.core :as cheshire]
+            [cheshire.custom :as cust]))
+
+(cust/add-encoder java.lang.Class
+                  (fn [c jsonGenerator]
+                    (.writeString jsonGenerator (-> c .getSimpleName))))
+
+
 
 (defn build-app []
   (-> {:swagger
        {:ui   "/swagger/ui"
         :spec "/swagger.json"
-        :data {:info {:title       "Voltron 42 Webserver"
-                      :description "trying something out"}
-               :tags [{:name "Hello World", :description "Simple sample http routing"}]}}}
+        :data {:info {:title       "Facetious Nocturn"
+                      :description "Shared Session Server"}
+               :tags [{:name "Host", :description "functions for the Session Host"}
+                      {:name "Guest", :description "functions for Session Guests"}]}}}
       (sweet/api
         (api/context
           "/api/v1" []
-          :tags ["Hello World"]
           (api/context
-            "/hi-there" []
+            "/host" []
+            :tags ["host"]
             (sweet/resource
-              {:description ""
-               :get         {:summary    ""
-                             :parameters {:query-params s/Any}
-                             :produces   ["application/json"]
-                             :responses  {200 {}}
-                             :handler    (fn [{:keys [query-params]}]
-                                           (http/ok query-params))}})))
+             {:description ""
+              :post         {:summary    ""
+                            :parameters {:body s/Session}
+                            :consumes   ["application/json"]
+                            :produces   ["application/json"]
+                            :responses  {200 s/Session}
+                            :handler    (fn [{:keys [body] :as req}]
+                                          (log/info (cheshire/generate-string
+                                                     (:keys req)
+                                                     {:pretty true}))
+                                          (http/ok req))}})))
         (sweet/GET "/" [] (resp/redirect "/index.html")))
       (sweet/routes
         (route/resources "/")
